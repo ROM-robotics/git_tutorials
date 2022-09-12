e8b743ef43e442277fb582e0db5dcb5ef4c19ed2
Pyae Soan Aung <romrobotics@gmail.com>
2022-09-12 08:56:24 +0630

    third

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-/sdjfl;sjf;sl
+#include <iostream>
+using namespace std;
+
+int factorial(int num);
+
+int main()
+{
+	int num=0;
+
+	do{
+	std::cout<< "enter positive integer: "<< std::endl;
+	cin>>num;
+	}while(num<0);
+	
+	int factorial;
+	for(int i=1;i<=num;i++)
+	{
+		factorial = factorial *i;
+		}
+		
+	std::cout<< num << " != " << factorial << std::endl;
+	return 0;
+}
